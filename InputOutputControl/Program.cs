4855f0c96e1869a603e7e3e4d889a8e67e07308b
Valacor <valyrious.informatics@gmail.com>
2024-08-01 22:37:06 +0200

    Create project for input and output handling

---
+++ InputOutputControl/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace InputOutputControl;
+
+/// <summary>
+/// Executes io operations
+/// </summary>
+internal class Program
+{
+    public static async Task Main(string[] args)
+    {
+
+    }
+}
