b1a8b59f45a29c6aebdebbbb1d296f359bb4675f
Valacor <valyrious.informatics@gmail.com>
2024-08-01 22:38:59 +0200

    Tests for the codebase

---
+++ CodebaseTests/ProtocolReadingTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodebaseTests;
+
+public class ProtocolReadingTests
+{
+    [Fact]
+    public void ReadByteOrderCorrectly()
+    {
+        
+    }
+
+}
---
+++ CodebaseTests/ProtocolWritingTests.cs
@@ -0,0 +1,33 @@
+﻿using Codebase.Communication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodebaseTests;
+
+public class ProtocolWritingTests
+{
+    [Theory]
+    [InlineData(0)]
+    [InlineData(1)]
+    [InlineData(2)]
+    [InlineData(3)]
+    [InlineData(4)]
+    [InlineData(5)]
+    [InlineData(6)]
+    [InlineData(7)]
+
+    public void ThrowWhenBufferToSmall(int count)
+    {
+        CommPyMessage msg = new CommPyMessage();
+        Span<byte> buffer = new byte[count];
+    }
+
+    [Fact]
+    public void SuccessfulWriteMessageToBuffer()
+    {
+
+    }
+}
